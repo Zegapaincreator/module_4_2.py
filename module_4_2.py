e52bebc511f09227878197e4cfb71a5518a45ca3
Zegapaincreator <goohagis2@gmail.com>
2024-10-31 23:05:54 +0700

    Add files via upload

--- module_4_2.py
+++ module_4_2.py
@@ -1,10 +1,7 @@
 def test_function():
-    print('Внешняя функция')
+    def inner_function():
+        print(f"Я в области видимости функции inner_function()")
+
     inner_function()
 
-def inner_function():
-    print('Я в области видимости функции test_function')
-
-
-test_function()
-#inner_function()
+inner_function()
